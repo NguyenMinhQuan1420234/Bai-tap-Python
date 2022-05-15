526f1463a2c56553edaf5b81e49af32e822f9310
Nguyễn Minh Quân <35888563+NguyenMinhQuan1420234@users.noreply.github.com>
2022-05-16 00:29:58 +0700

    update bai 7

---
+++ bai7/7.1.py
@@ -0,0 +1,12 @@
+x = [str(x) for x in input("List of animals: ").split(",")]
+print("Number of list is: ", x) 
+print("Numbers of animal: ",len(x))
+
+tim = input("I want to find: \n")
+
+for char in x:
+    if char == tim:
+        print(f"there is {tim} in list of animals")
+        break
+else:
+    print(f"there is no {tim} in list of animals")
