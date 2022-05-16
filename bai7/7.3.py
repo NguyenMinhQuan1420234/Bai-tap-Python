b50d321819aec73f2c9a667ac5cd4ffa507f3b1c
Nguyễn Minh Quân <35888563+NguyenMinhQuan1420234@users.noreply.github.com>
2022-05-17 01:36:45 +0700

    sa

--- bai7/7.3.py
+++ bai7/7.3.py
@@ -12,19 +12,34 @@ while x == True:
 MangSoNT = []
 MangDuong = []
 MangAm = []
+check = 0
 for a in thislist:
-    if a >= 0:
-        MangDuong.append(a)
-    else:
+    if a < 0:
         MangAm.append(a)
-    for i in range(2,a+1,1):
-        if a%i != 0:
-            pass
-        else:
-            break
-    if a==i:
-        MangSoNT.append(a)
+    elif a > 0:
+        MangDuong.append(a)
+        for i in range(2,a+1,1):
+            if a % i != 0:
+                pass
+            else:
+                break
+            check = i
+        if a == check or a == 2:
+            MangSoNT.append(a)
+        
 print("Cac so nguyen to trong List la: ", MangSoNT)
 print("Cac phan tu am cua List: ",MangAm)
 print("Cac phan tu duong cua List: ",MangDuong)
 
+tbc = 0
+for a in MangDuong:
+    tbc += a
+if len(MangDuong) > 0 :
+    print("Trung binh cong cac phan tu duong la: ", tbc/len(MangDuong))
+tbc = 0
+for a in MangAm:
+    tbc += a
+if len(MangAm) > 0 :
+    print("Trung binh cong cac phan tu am la: ", tbc/len(MangAm))
+
+print("List sap tang dan: ", sorted(thislist))
